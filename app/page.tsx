3f4f641994c184aca7064a09c28c9de703183938
Agustin Menchon <agus.menchon@gmail.com>
2026-09-12 16:05:52 +0800

    feat: agregar soporte para análisis de Vercel en la página de inicio

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next"
 import { Site } from "@/components/site";
 import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Site />
       <Footer />
     </>
